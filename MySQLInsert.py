a02a16398a1cae3d46fcb6989d2ba9a05c8c834e
walker <34888260+Liuteccc@users.noreply.github.com>
2019-12-03 13:54:16 +0800

    Update MySQLInsert.py

--- MySQLInsert.py
+++ MySQLInsert.py
@@ -16,14 +16,17 @@ def monsterData():
     for i in range(len(data)):
         #每循环一次，记录时间增加一分钟
         timeMark = (datetime.datetime(2019, 10, 1, 00, 00, 00) + datetime.timedelta(minutes=i)).strftime("%H%M")
-        #插入指定数据
+        #每循环一次，插入一条数据
         sqlInsert = "INSERT INTO December(monsterID,recordID,week,date)VALUES(%s,%s,%s,%s)"%(data[i], '"'+timeMark+'0'+data[i]+'"','1','20191202')
 
         try:
             cursor.execute(sqlInsert)
             db.commit()
+            
+        # 出错回滚
         except:
             db.rollback()
+    #关闭数据库连接
     db.close()
 
 # 时间初始格式模板
@@ -31,3 +34,5 @@ def monsterData():
 
 # 执行方法
 monsterData()
+
+# 注意：注释基本添加于GitHub，可能存在缩进问题。
