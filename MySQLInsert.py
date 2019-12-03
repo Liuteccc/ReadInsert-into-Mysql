9562d039826cffb006e05f4f02ecdb7586b237ad
walker <34888260+Liuteccc@users.noreply.github.com>
2019-12-03 13:45:25 +0800

    Update MySQLInsert.py

--- MySQLInsert.py
+++ MySQLInsert.py
@@ -3,15 +3,20 @@ import io
 import datetime
 import MySQLdb
 
+# 打开本地文件
 f = io.open("../Data/1202塔防数据统计.txt", "r", encoding="utf-8")
 
+# 定义一个方法
 def monsterData():
     db = MySQLdb.connect("localhost", "root", "mysql123", "Monster", charset='utf8')
     cursor = db.cursor()
 
+    #用空格切割数据
     data = str(f.read()).split(' ')
     for i in range(len(data)):
+        #每循环一次，记录时间增加一分钟
         timeMark = (datetime.datetime(2019, 10, 1, 00, 00, 00) + datetime.timedelta(minutes=i)).strftime("%H%M")
+        #插入指定数据
         sqlInsert = "INSERT INTO December(monsterID,recordID,week,date)VALUES(%s,%s,%s,%s)"%(data[i], '"'+timeMark+'0'+data[i]+'"','1','20191202')
 
         try:
@@ -24,4 +29,5 @@ def monsterData():
 # 时间初始格式模板
 # n = (datetime.datetime(2019, 10, 1, 23, 59, 00)+datetime.timedelta(minutes=1)).strftime("%Y-%m-%d %H:%M:%S")
 
+# 执行方法
 monsterData()
